8b53096df5dbef4e919745b1b326bd75d06d26d3
Tyler0130 <rocket0191@gmail.com>
2018-05-28 02:25:17 -0500

    Set up notFoundHandler and view engine.

--- app/start.php
+++ app/start.php
@@ -8,4 +8,24 @@ $app = new \Slim\App([
     ]
 ]);
 
+$c = $app->getContainer();
+
+$c['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('The page you were looking for was not found.');
+    };
+};
+
+$c['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('src/views', ['cache' => false]);
+
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new \Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
 require __DIR__ . '/routes.php';
