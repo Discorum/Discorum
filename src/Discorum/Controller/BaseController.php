9e3f5bbea410cc01c3dae1c69cfd03a0aa4ff58b
Nicholas Sylke <nicksylke@outlook.com>
2018-05-30 22:25:15 -0500

    Add documentation to BaseController

--- src/Discorum/Controller/BaseController.php
+++ src/Discorum/Controller/BaseController.php
@@ -2,19 +2,38 @@
 
 namespace Discorum\Controller;
 
+use Psr\Container\ContainerInterface;
+
 abstract class BaseController
 {
+    /**
+     * The container interface
+     * 
+     * @var \Psr\Container\ContainerInterface
+     */
     protected $container;
 
-    public function __construct($container)
+    /**
+     * Constructor
+     * 
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
+    /**
+     * Retrieve container items
+     * 
+     * @param string $property The property item
+     */
     public function __get($property)
     {
-        if ($this->container->{$property}) {
-            return $this->container->{$property};
+        if ($this->container->has($property)) {
+            return $this->container->get($property);
         }
+
+        trigger_error('Undefined property: ' . $property);
     }
 }
